da1d43650d42ceea939be1370c6eee1b127c0f12
ksg-dev <ksg.dev.data@gmail.com>
2026-07-02 12:10:52 -0400

    add lists admin models

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import NamedList, ListItem
 
-# Register your models here.
+
+class ListItemInline(admin.TabularInline):
+    model = ListItem
+    extra = 0
+
+
+@admin.register(NamedList)
+class NamedListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    inlines = [ListItemInline]
+
+
+@admin.register(ListItem)
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = ('text', 'list', 'is_complete', 'created_at')
+    list_filter = ('list', 'is_complete')
